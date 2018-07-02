2253369f5bffd7d7460aa9b9ff7e1ee2fa96e44a
Bobby Martin <greendolphinavenue@gmail.com>
2018-07-02 20:07:30 +0000

    put stuff in constants and retry connnection every 5 seconds

--- __main__.py
+++ __main__.py
@@ -1,19 +1,36 @@
 from picamera import PiCamera
-from socket import socket
+from socket import socket, error
+from time import sleep
 
+# constants for video stuff
+width = 1280
+height = 720
+framerate = 32
+
+# constants/variables for connection stuff
 HOST = 'localhost'
 PORT = 8082
+connected = False
 
 # socket to thing we're sending video shit to
 client_socket = socket()
-client_socket.connect((HOST, PORT))
+# retry connection every 5 seconds until success
+while not connected:
+    try:
+        client_socket.connect((HOST, PORT))
+        connected = True
+    except error:
+        print 'Connection to {}:{} failed, trying again in 5 seconds.'.format(HOST, PORT)
+        sleep(5)
+
+# this gets passed to the video function to be used as the destination for the stream
 connection = client_socket.makefile('wb')
+print 'Connected to master process!'
 
 with PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.framerate = 1
+    camera.resolution = (width, height)
+    camera.framerate = framerate
     camera.start_recording(connection, format='mjpeg')
-    camera.wait_recording(60)
     camera.wait_recording(60)
     camera.stop_recording()
 
