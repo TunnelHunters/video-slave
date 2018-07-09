f6bb3e8cff0ed4a2c3052e2d7114af2fb722f980
CALICAWESOME <greendolphinavenue@gmail.com>
2018-07-08 23:39:11 -0400

    make server back into client

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import atexit
-from os import unlink, path
-from socket import socket, AF_UNIX, SOCK_STREAM
+from os import environ
+from socket import socket, AF_UNIX, SOCK_STREAM, error
+from time import sleep
 from picamera import PiCamera
 
 # constants for video stuff
@@ -8,43 +9,36 @@ width = 854
 height = 480
 framerate = 32
 # path to unix socket
-socket_path = '/tmp/THZ_video.sock'
+socket_path = environ['THZ_videoSocket']
 
 
 @atexit.register
 def on_exit():
     try:
-        client_fd.close()
-        client.close()
+        sock_fd.close()
+        sock.close()
     except NameError:
         pass
 
-    delete_socket()
-    print 'goodbye!!'
-
-
-def delete_socket():
-    if path.exists(socket_path):
-        unlink(socket_path)
+    print 'Goodbye!!'
 
 if __name__ == '__main__':
-    delete_socket()
     # make & bind socket then listen for connections
     sock = socket(AF_UNIX, SOCK_STREAM)
-    sock.bind(socket_path)
-    sock.listen(1)
-    print 'listening at {}'.format(socket_path)
+    try:
+        sock.connect(socket_path)
+    except error:
+        print 'Connection to robot-master failed, trying again in 5 seconds.'
+        sleep(5)
 
-    # accept a connection
-    client, _ = sock.accept()
-    client_fd = client.makefile()
-    print 'somebody connected!!'
+    print 'Connected to {}!'.format(socket_path)
+    sock_fd = sock.makefile()
 
     # PiCamera stuff
     with PiCamera() as camera:
         camera.resolution = (width, height)
         camera.framerate = framerate
-        camera.start_recording(client_fd, format='mjpeg')
+        camera.start_recording(sock_fd, format='mjpeg')
         # TODO: How do I record forever?
         camera.wait_recording(60)
         camera.stop_recording()
